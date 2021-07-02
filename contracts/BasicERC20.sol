026c1f09cf89e7cdbe74eff42c7c162b1bfbed12
Gowtham Paramasivam <gowthamparamasivam66@gmail.com>
2021-07-02 11:30:47 +0530

    adding linters

--- contracts/BasicERC20.sol
+++ contracts/BasicERC20.sol
@@ -5,8 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BasicERC20 is ERC20{
 
-    constructor () ERC20("BASIC1", "BAS") {
+    constructor() public ERC20("BASIC1", "BAS") {
         _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
+    }
+
+        function sumaThan() public returns (string){
+return new string("hello");
     }
 
 }
